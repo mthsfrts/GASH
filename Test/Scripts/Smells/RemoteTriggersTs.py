2b6ecb53ab4c05c54afb184c4afed057f0f75c29
aluiziodev <aluneto2000@gmail.com>
2026-01-09 08:45:01 -0300

    Ajustando findings incompativeis e adicionando workflow_calls invalidos

--- Test/Scripts/Smells/RemoteTriggersTs.py
+++ Test/Scripts/Smells/RemoteTriggersTs.py
@@ -76,27 +76,30 @@ def test_remote_integration(workflow):
 
     expected_findings = [
         "Input 'call_input_03' has an invalid type 'invalid_type'.",
-        "Input 'call_input_03' lacks a description. Consider add a description for a "
-        'better understanding and maintenance.',
+        "Input 'call_input_03' lacks a description. Consider adding a "
+        "description for better understanding and maintenance.",
         "Input 'call_input_04' is required but has no default value.",
         "Input 'call_input_08' is required but has no default value.",
         "Input 'call_input_11' is required but has no default value.",
-        "Input 'call_input_13' of type 'boolean' should not be required. Consider "
-        'remove the parameter.',
+        "Input 'call_input_13' of type 'boolean' should not be required. "
+        "Consider removing the parameter.",
         "Input 'call_input_16' is required but has no default value.",
-        'The trigger has too many inputs. Consider revisit your original Action to '
-        'see the need for all of the inputs. The inputs overflow, might cause '
-        'security and maintenance issues.',
+        "Input 'call_input_17' of type 'choice' lacks an 'options' definition.",
+        'Invalid configuration for workflow_dispatch: expected a dictionary, '
+        'but got NoneType. Ensure the workflow_dispatch configuration is properly defined.',
+        'The trigger has too many inputs. Consider revisiting your original Action to see '
+        'the need for all of the inputs. An overflow of inputs might cause security and '
+        'maintenance issues.',
         'Workflow call trigger is set with a higher permission on a workflow level. '
-        'Consider the add best security protocol for it. This trigger might harm your '
-        'pipeline if it is not configure correctly.',
-        "Workflow-run has both 'branches' and 'branches-ignore' defined. If you want "
-        'to both include and exclude branch patterns for a single event, use the '
-        "branches filter along with the '!' character to indicate which branches "
-        'should be excluded. The misconfiguration of it might cause you issues but '
-        'not a directly security one.',
-        'You should be careful when reference secrets in workflow call. Consider use '
-        'Secrets Env to do so. Ex: ${{ secrets.SECRET_NAME }}.'
+        'Consider the add best security protocol for it. This trigger might harm '
+        'your pipeline if it is not configured correctly.',
+        "Workflow-run has both 'branches' and 'branches-ignore' defined. "
+        "If you want to both include and exclude branch patterns for a single event, "
+        "use the branches filter along with the '!' character to indicate which branches"
+        " should be excluded. The misconfiguration of this might cause issues, but not directly "
+        "security-related ones.",
+        'Workflow-run lacks a workflow. You need to add an event to trigger the run parameter.',
+        'You should be careful when referencing secrets in workflow call. '
+        'Consider using Secrets Env to do so. Ex: ${{ secrets.SECRET_NAME }}.'
     ]
-
     assert sorted(findings) == sorted(expected_findings)
